71d6a1dd2c299a49bb75f7e279649d6961a449d0
kleo-xhindoli <kleo.xhindoli@gmail.com>
2022-04-13 15:19:52 +0200

    simplify the app even more

--- cypress/integration/app/app.spec.js
+++ cypress/integration/app/app.spec.js
@@ -1,27 +1,13 @@
 /// <reference types="cypress" />
 
 describe("Testing a submitting select box", () => {
-  // This fails in Chrome >= 83, but succeeds in Chrome 80 and in Firefox
-  it("no explicit wait, this will fail but shouldn't", () => {
+  it("should show the selected user", () => {
     cy.visit("http://localhost:3000");
-    cy.get("input").type("ervin");
-    //cy.wait(0)
+
+    cy.wait(200);
     cy.contains("button", "Ervin Howell").click();
     cy.get("[data-testid='selected-user']")
       .should("be.visible")
       .should("contain.text", "Ervin Howell");
   });
-  // skip("explicit wait after submit, this will succeed", () => {
-  //   cy.visit("http://localhost:3000");
-  //   cy.get("select").select("Second");
-  //   cy.wait(1000);
-  //   cy.get("input").type("Hallo");
-  // });
-
-  // skip("even a wait(0) will do", () => {
-  //   cy.visit("http://localhost:3000");
-  //   cy.get("select").select("Second");
-  //   cy.wait(1000);
-  //   cy.get("input").type("Hallo");
-  // });
 });
